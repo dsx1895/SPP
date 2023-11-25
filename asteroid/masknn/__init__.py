c57eb58ae428b26410e54289b4cfed6377feeb3b
dsx1895 <42482051+dsx1895@users.noreply.github.com>
2023-11-25 15:29:01 +0900

    Update __init__.py

--- asteroid/masknn/__init__.py
+++ asteroid/masknn/__init__.py
@@ -1,7 +1,5 @@
-from .convolutional import TDConvNet
+# from .convolutional import TDConvNet
 from .recurrent import DPRNN
-from .attention import DPTransformer
-from .networks import PatchSampleF
 
 __all__ = [
     "TDConvNet",
